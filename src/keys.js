61b56ba95439a557c94a0c8497e64f520e28dc84
vin-din <116697267+vin-din@users.noreply.github.com>
2023-05-15 04:26:29 -0700

    cambios

--- src/keys.js
+++ src/keys.js
@@ -3,6 +3,7 @@ module.exports = {
         host: process.env.DB_HOST || 'localhost',
         user: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || 'root',
-        database: process.env.DB_NAME || 'data'
+        database: process.env.DB_NAME || 'data',
+        port: process.env.DB_PORT || 3306
     }
 };
